4a58b5c914fd636f2984f41dab8b79b320ec5aa5
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2020-12-27 18:20:17 -0600

    Added back stack manipulation with popTo exclusive and added onClick handlers to the buttons on the end states.

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -16,23 +16,31 @@
 
 package com.example.android.navigation
 
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_over, container, false)
+            inflater, R.layout.fragment_game_over, container, false
+        )
         // TODO (05) Add an onClick Handler for the tryAgain button that navigates to the gameFragment
         // using action_gameOverFragment_to_gameFragment
+        binding.tryAgainButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                R.id.action_gameOverFragment_to_gameFragment
+            )
+        )
         return binding.root
     }
 }
--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,24 +16,14 @@
 
 package com.example.android.navigation
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.core.app.ShareCompat
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.navigation.findNavController
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentGameWonBinding
-import android.content.pm.ResolveInfo
-import android.content.pm.PackageManager
-
 
 
 class GameWonFragment : Fragment() {
@@ -41,9 +31,15 @@ class GameWonFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_won, container, false)
+            inflater, R.layout.fragment_game_won, container, false
+        )
         // TODO (06) Add an onClick Handler for the nextMatch button that navigates to the gameFragment
         // using action_gameWonFragment_to_gameFragment
+        binding.nextMatchButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                R.id.action_gameWonFragment_to_gameFragment
+            )
+        )
         return binding.root
     }
 }
